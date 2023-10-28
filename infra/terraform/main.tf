f6c2c7f8a0429165d6d8ec498a6d648e6b39383e
Murylo Capucho <mcapuchoribeiro@gmail.com>
2023-10-28 01:10:43 -0300

    Implementação da Infra S3

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -4,7 +4,7 @@ resource "aws_s3_bucket" "mixfast_s3_bucket" {
   tags = var.tags
 }
 
-resource "aws_s3_bucket" "mixfast_s3_bucket" {
+resource "aws_s3_bucket" "mixfast_s3_bucket_ecs" {
   bucket = "${var.name}-s3-bucket-ecs"
 
   tags = var.tags
